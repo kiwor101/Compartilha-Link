e20400e6e485155e0461d0508e8f7fc25a691beb
kiwor101 <ribeiro10@outlook.com>
2026-05-19 15:50:27 -0300

    1

--- static/config.js
+++ static/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  microsoftClientId: "cole-aqui-o-client-id-do-app",
-  microsoftTenantId: "cole-aqui-o-tenant-id",
+  microsoftClientId: "03b8c0d8-3d68-44c2-a2f6-18c09d7c3bca",
+  microsoftTenantId: "251017e3-b3a8-40f0-b6fe-57b159bcc5d8",
   uploadRootFolder: "Compartilhamentos Externos"
 };
